7a35a4048fef0cf73fb7b1dc5d93fc984975eb31
Moritz Peters <maritz.peters@gmail.com>
2016-09-01 01:51:44 +0200

    Remove stream and readline functionality, now just strings

--- index.ts
+++ index.ts
@@ -3,19 +3,13 @@ import * as fs from 'fs';
 import parseLog from './lib/index';
 
 if (require.main === module) {
-  parseLog(fs.createReadStream('testlog.txt'))
-    .then(
-      (result) => {
-        const game = result.games[5];
-        console.log('players', game.players);
-        console.log(`Won ${result.summary.wins} out of ${result.summary.wins + result.summary.losses} games.`);
 
-        //console.log(JSON.stringify(result));
-      },
-      (error) => {
-        console.log('error', error);
-      }
-    );
+  const result = parseLog(fs.readFileSync('testlog.txt', 'utf-8'));
+  const game = result.games[5];
+  console.log('players', game.players['Trix4er'].melee);
+  console.log(`Won ${result.summary.wins} out of ${result.summary.wins + result.summary.losses} games.`);
+
+  //console.log(JSON.stringify(result));
 }
 
 export default parseLog;
--- lib/index.ts
+++ lib/index.ts
@@ -1,90 +1,76 @@
-import * as readline from 'readline';
-import * as streamify from 'streamifier';
-import * as isStream from 'is-stream';
-
 import DamageSummary from './damageSummary';
 import LogEntry from './logEntry';
 import Game from './game';
 
 
-export default function parseLog(input: any): Promise<CullingParser.parseLogResponse> {
+export default function parseLog(input: string): CullingParser.parseLogResponse {
 
-  return new Promise<CullingParser.parseLogResponse>((resolve, reject) => {
-    const modules: Array<string> = [];
-    const response = {
-      meta: {
-        lines: {
-          total: 0,
-          relevant: 0
-        }
-      },
-      entries: <Array<LogEntry>>[],
-      games: <Array<any>>[],
-      summary: {
-        wins: 0,
-        losses: 0,
-        kills: 0,
-        deaths: 0,
-        damage: <CullingParser.IDamageSummary>{}
+  const modules: Array<string> = [];
+  const response = {
+    meta: {
+      lines: {
+        total: 0,
+        relevant: 0
       }
-    };
-    const damageSummary = new DamageSummary();
-    let currentGame = new Game();
-
-    let inputStream = input;
-    if (! isStream.readable(inputStream)) {
-      inputStream = streamify.createReadStream(input);
+    },
+    entries: <Array<LogEntry>>[],
+    games: <Array<any>>[],
+    summary: {
+      wins: 0,
+      losses: 0,
+      kills: 0,
+      deaths: 0,
+      damage: <CullingParser.IDamageSummary>{}
     }
-    const byLine = readline.createInterface({ input: inputStream });
+  };
+  const damageSummary = new DamageSummary();
+  let currentGame = new Game();
 
+  const lines = input.split('\n');
+  lines.forEach((line) => {
+    response.meta.lines.total++;
+    const entry = new LogEntry(line);
 
-    byLine
-      .on('line', (line: string) => {
-        response.meta.lines.total++;
-        const entry = new LogEntry(line);
+    if (modules.indexOf(entry.moduleName) === -1) {
+      modules.push(entry.moduleName);
+    }
 
-        if (modules.indexOf(entry.moduleName) === -1) {
-          modules.push(entry.moduleName);
-        }
+    entry.parse();
+    if (!entry.interesting) {
+      return;
+    } else {
+      response.meta.lines.relevant++;
+    }
 
-        entry.parse();
-        if (!entry.interesting) {
-          return;
-        } else {
-          response.meta.lines.relevant++;
-        }
+    if (entry.isKill) {
+      response.summary.kills++;
+    }
+    if (entry.isDeath) {
+      response.summary.deaths++;
+    }
+    if (entry.isWin) {
+      response.summary.wins++;
+    }
+    if (entry.isLoss) {
+      response.summary.losses++;
+    }
 
-        if (entry.isKill) {
-          response.summary.kills++;
-        }
-        if (entry.isDeath) {
-          response.summary.deaths++;
-        }
-        if (entry.isWin) {
-          response.summary.wins++;
-        }
-        if (entry.isLoss) {
-          response.summary.losses++;
-        }
+    damageSummary.add(entry.damage);
+    currentGame.addEntry(entry);
+    if (currentGame.isFinished) {
+      response.games.push(currentGame.getResult());
+      currentGame = new Game();
+    }
 
-        // damage
-        damageSummary.add(entry.damage);
-        currentGame.addEntry(entry);
-        if (currentGame.isFinished) {
-          response.games.push(currentGame);
-          currentGame = new Game();
-        }
+    response.entries.push(entry);
+  });
 
-        response.entries.push(entry);
-      })
-      .on('close', () => {
-        if (currentGame.start && !currentGame.isFinished) {
-          // last game didn't finish according to logs. crashed?
-          currentGame.finish(response.entries[response.entries.length - 1]);
-          response.games.push(currentGame);
-        }
-        response.summary.damage = damageSummary.getSummary();
-        resolve(response);
-      });
-  })
+  if (currentGame.start && !currentGame.isFinished) {
+    // last game didn't finish according to logs. crashed?
+    currentGame.finish(response.entries[response.entries.length - 1]);
+    response.games.push(currentGame);
+  }
+  response.summary.damage = damageSummary.getSummary();
+  return response;
+
 }
