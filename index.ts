c2a6c221ab9e2d55cf8f1b8df5e4b903a9f6108f
Moritz Peters <maritz.peters@gmail.com>
2016-09-01 02:02:28 +0200

    Remove temporary debug code

--- index.ts
+++ index.ts
@@ -1,15 +1,3 @@
-import * as fs from 'fs';
-
 import parseLog from './lib/index';
-
-if (require.main === module) {
-
-  const result = parseLog(fs.readFileSync('testlog.txt', 'utf-8'));
-  const game = result.games[5];
-  console.log('players', game.players['Trix4er'].melee);
-  console.log(`Won ${result.summary.wins} out of ${result.summary.wins + result.summary.losses} games.`);
-
-  //console.log(JSON.stringify(result));
-}
 
 export default parseLog;
